accb73149e2030651aca7125ea9ea71de1164d8e
Hamza Alaoui <53179016+HamzaOPLEX@users.noreply.github.com>
2020-02-08 14:47:29 +0100

    Create DateFromatCorector.py

---
+++ DateFromatCorector.py
@@ -0,0 +1,33 @@
+import re,pyperclip
+
+'''
+        Simple Algorithm :
+                1 - After copy you text distination store it in variable named text
+                2 - use this ReEx [\d]{1,2}[/-][\d]{1,2}[/-][\d]{4} to search DATE format that conatin / - in text variable and store them in a List with name of REsearch
+                3 - search Values in REsearch that in DATE format aa/aa/aaaa and store them in a list REsearch_i
+                4 - change list REsearch_i values fromat from aa/aa/aaaa to aa-aa-aaaa and save them in a list with name REsub_j
+                5 - take the REsub_j values and change them in text variable them copy text to clipbloard
+'''
+
+
+#1
+text = pyperclip.paste()
+
+#2
+REcompile = re.compile(r"[\d]{1,2}[/-][\d]{1,2}[/-][\d]{4}",re.DOTALL) #search DATE RegEX
+REsearch = REcompile.findall(text) #Findall DATE fromat
+
+#3
+for i in REsearch :
+        REcompile_i = re.compile(r"[\d]{1,2}[/][\d]{1,2}[/][\d]{4}") #Search DATE format that contain / in REsearch
+        REsearch_i = REcompile_i.findall(i) #Findall 
+
+#4
+        for j in REsearch_i : 
+                REcompile_j = re.compile("/")  #search in REsearch_i for /
+                REsub_j = REcompile_j.sub("-",j)     # and change it to -
+                print(REsub_j)
+#5
+                REcompile_text = re.compile(j) #search for the Normal Value that contain / in text
+                REsub_text = REcompile_text.sub(REsub_j,text)  #change it with the new Value that contain -
+                Copy_RE = pyperclip.copy(REsub_text) # #Copy it to ClipBoard
